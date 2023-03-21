5fbc5d656301ee0799cb654f8fd4fb11a049bf19
morehit <95517329+morehit@users.noreply.github.com>
2023-03-21 18:45:50 +0530

    Add files via upload

---
+++ read.py
@@ -0,0 +1,25 @@
+import cv2 as cv
+
+# img  = cv.imread('images\wallpaperflare.com_wallpaper (3).jpg')
+
+# cv.imshow('Cat' ,img)
+
+# cv.waitKey(0)
+
+
+
+capture = cv.VideoCapture('videos/video.mp4')
+
+while True:
+    isTrue , frame = capture.read()
+    cv.imshow('Video', frame)
+    
+    if cv.waitKey(20) & 0xFF == ord('d'):
+        break
+
+capture.release()
+cv.destroyAllWindows()
+
+
+
+
