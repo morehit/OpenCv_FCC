c73f79c65de15e57cf2e1efa159e15ee0cfc6bb2
Mohit Pargaie <mohitpargaie01@gmail.com>
2023-04-04 10:36:01 +0530

    04-04-23

--- bitwise.py
+++ bitwise.py
@@ -4,7 +4,20 @@ import numpy as np
 blank = np.zeros((400, 400), dtype='uint8')
 
 rectangle = cv.rectangle(blank.copy(), (30,30), (370,370), 255, -1)
+cv.imshow('Rectangle' , rectangle)
+circle = cv.circle(blank.copy() , (200, 200 ) , (200) , 255 ,  -1 )
+cv.imshow('Circle' , circle )
 
+bitwise_and = cv.bitwise_and(rectangle , circle)
+cv.imshow('Bitwise_and' , bitwise_and )
 
+bitwise_or = cv.bitwise_or(rectangle , circle)
+cv.imshow('Bitwise-or ', bitwise_or)
+
+bitwise_xor = cv.bitwise_xor(rectangle , circle )
+cv.imshow('Bitwise_xor' , bitwise_xor)
+
+bitwise_not = cv.bitwise_not(circle)
+cv.imshow('Bitwise_not' , bitwise_not)
 
 cv.waitKey(0)
